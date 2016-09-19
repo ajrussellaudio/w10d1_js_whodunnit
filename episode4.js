1f603f139de8e8538eaa2a6a952f797c3d554c98
Alan Russell <ajrussellaudio@gmail.com>
2016-09-19 17:27:46 +0100

    correct episode 4

--- episode4.js
+++ episode4.js
@@ -13,3 +13,9 @@ console.log( 'Suspect three is:' + suspectThree )
 
 // 'Suspects include: Jay, Val, Harvey, Rick'
 // 'Suspect three is: Harvey'
+// wrong!!
+//
+// var suspectThree = 'Harvey'
+// is limited in scope to the allSuspects function
+
+// Suspect three is:Keith
