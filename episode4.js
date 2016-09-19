da41e1ecc86b312a33ff6147ae8c38185d9a62ed
Alan Russell <ajrussellaudio@gmail.com>
2016-09-19 17:22:44 +0100

    episode 4

---
+++ episode4.js
@@ -0,0 +1,15 @@
+var suspectOne = 'Jay';
+var suspectTwo = 'Val';
+var suspectThree = 'Keith';
+var suspectFour = 'Rick';
+
+var allSuspects = function() {
+    var suspectThree = 'Harvey'
+    console.log('Suspects include: ' + suspectOne + ', ' + suspectTwo + ', ' + suspectThree + ', ' + suspectFour)
+}
+
+allSuspects();
+console.log( 'Suspect three is:' + suspectThree )
+
+// 'Suspects include: Jay, Val, Harvey, Rick'
+// 'Suspect three is: Harvey'
