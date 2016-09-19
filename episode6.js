fe1eebddadcfc9a84da821470ab19c0d0124c572
Alan Russell <ajrussellaudio@gmail.com>
2016-09-19 17:25:25 +0100

    episode 6

---
+++ episode6.js
@@ -0,0 +1,16 @@
+var murderer = 'rick';
+
+var outerFunction = function(){
+    var murderer = 'marc';
+
+    var innerFunction = function(){
+        murderer = 'valerie';
+    }
+
+    innerFunction();
+}
+
+outerFunction();
+console.log('the murderer is ', murderer);
+
+// valerie
